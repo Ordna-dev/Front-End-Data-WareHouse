48af9f7c7e331bb337f1d64b0e5de54984bf6246
Ordna-dev <ordnajelaorerreug@gmail.com>
2022-11-27 19:50:34 -0600

    Proyecto de Data WareHouse

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
 
 function App() {
+
+  const [message, setMessage] = useState(null);
+
+  const mensaje = async () => {
+    try {
+      let res = await axios.get('/api');
+      let resultado = res.data;
+      setMessage(resultado)
+    } catch(e) {
+      console.log(e)
+    }
+  }
+  useEffect(() => {
+    mensaje()
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      {message}
     </div>
-  );
+  )
 }
 
 export default App;
